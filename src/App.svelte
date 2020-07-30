ec7c92e30ee7b6df59204ef6ac0f60e27cb28143
Stiv <pabloml@kzsoftworks.com>
2020-07-29 23:33:02 -0300

    Finish animation and add background

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,37 @@
 <script>
+  import Recipe from "./Recipe.svelte";
+
   const screens = {
     home: "home",
     recipe: "recipe",
   };
-  let hovered = false;
-  let screen = screens.home;
+  let screen = screens.recipe;
+  let getNewRecipe = false;
 </script>
 
 <style>
   :global(body) {
     padding: 0;
+    overflow: hidden;
+  }
+  :global(body::after) {
+    content: "";
+    position: absolute;
+    width: 200%;
+    height: 200%;
+    top: -50%;
+    left: -50%;
+    z-index: -1;
+    background-image: url("/imgs/taco.svg");
+    background-size: 124px;
+    background-position: 20 20;
+    background-repeat: space;
+    background-color: rgb(255, 247, 173);
+    transform: rotate(-45deg);
   }
   main {
-    background-color: rgb(255, 225, 144);
-    height: 100%;
-    width: 100%;
+    min-height: 100%;
+    min-width: 100%;
     padding: 0;
     margin: 0;
     display: flex;
@@ -55,8 +72,15 @@
 <main>
   {#if screen == screens.home}
     <h1>Taco Maker</h1>
-    <button>Get My Taco!</button>
-  {:else if screen == screens.recipe}
-    <div>recipe goes here</div>
+    <button on:click={() => (screen = screens.recipe)}>Get My Taco!</button>
+  {:else if screen == screens.recipe && !getNewRecipe}
+    <Recipe />
+    <button
+      on:click={() => {
+        getNewRecipe = true;
+        getNewRecipe = false;
+      }}>
+      Get New Recipe
+    </button>
   {/if}
 </main>
